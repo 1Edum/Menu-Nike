6f93a85e68dfb9b1a225c227c0dbda3e07c1e1f1
1Edum <eduardomatosdesouza2@gmail.com>
2024-03-19 18:33:28 -0300

    Cores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-    <div>
-      teste
-    </div>
+    <header>
+      <NavBar />
+    </header>
   );
 }
