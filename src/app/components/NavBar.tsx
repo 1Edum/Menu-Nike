d5c9fc91d4e95b4cd17a53e6772a0c697c24fe52
1Edum <eduardomatosdesouza2@gmail.com>
2024-03-19 19:17:36 -0300

    Icones Header

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,16 +1,42 @@
 import React from 'react'
+// Icons
 import { SiNike, SiJordan } from "react-icons/si";
+import { CiSearch, CiHeart  } from "react-icons/ci";
+import { BsBag } from "react-icons/bs";
 
 export function TopBar(){
     return(
-        <div className='flex justify-between items-center'>
+        <div className='flex-between-center bg-secondary'>
             <SiJordan className='hidden md:block size-5' />
-            <ul className='flex gap-5 text-sm'>
+            <ul className='flex gap-3 text-xs'>
                 <li>Nike Jornal</li>|
                 <li>Ajuda</li>|
                 <li>Junte-se a nós</li>|
                 <li>Entre</li>
             </ul>
+        </div>
+    )
+}
+export function MediumBar(){
+    return(
+        <div className='flex-between-center'>
+            <SiNike className='size-14'/>
+            <ul className='flex gap-3'>
+                <li>Lançamentos</li>
+                <li>Ofertas</li>
+                <li>Masculino</li>
+                <li>Feminino</li>
+                <li>infantil</li>
+                <li>SNKRS</li>
+            </ul>
+            <div className='flex'>
+                <div className='relative flex items-center'>
+                <CiSearch className='absolute'/>
+                <input type="text" placeholder='Buscar' className='bg-secondary' />
+                </div>
+                <CiHeart />
+                <BsBag />
+            </div>
         </div>
     )
 }
@@ -20,9 +46,7 @@ export default function NavBar() {
     <header>
         <nav>
             <TopBar />
-            <h1 className='font-nike'>
-                Nike
-            </h1>
+            <MediumBar />
         </nav>
     </header>
   )
